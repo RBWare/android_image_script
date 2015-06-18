9f902756e2a690bc964a13998bca923944db0dfb
Ryan Bailey <rbwaredev@gmail.com>
2015-06-18 11:47:52 -0500

    Much more thorough script, including args for width and height

--- image_script.sh
+++ image_script.sh
@@ -1,13 +1,64 @@
 #!/bin/sh
 
+usage() {
+    echo "Usage: $0 -w <int> -h <int> <input file.svg> <output filename.png>" 1>&2;
+    echo "Where the width and height are set for the MDPI baseline"
+    echo "Width and heigh are optional"
+    exit 1;
+}
+
+while getopts ":w:h:" o; do
+    case "${o}" in
+        w)
+            w=${OPTARG}
+            ;;
+        h)
+            h=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+        esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${w}" ] || [ -z "${h}" ]; then
+    w=48
+    h=48
+fi
+
+if [ -z "${1}"] || [ -z "${2}"]; then
+    usage
+fi
+
 echo "Creating directories"
 mkdir -p Images/mdpi/ Images/hdpi/ Images/xhdpi/ Images/xxhdpi/ Images/xxxhdpi/
 echo "Directory setup complete"
 
+# Set original w/h to reuse
+ow=$w
+oh=$h
+
 echo "Converting $1 to $2"
-convert -density 300 -background transparent $1 -resize 24x24 -gravity center -extent 24x24 Images/mdpi/$2 # 2>/dev/null
-convert -density 300 -background transparent $1 -resize 36x36 -gravity center -extent 36x36 Images/hdpi/$2 # 2>/dev/null
-convert -density 300 -background transparent $1 -resize 48x48 -gravity center -extent 48x48 Images/xhdpi/$2 # 2>/dev/null
-convert -density 300 -background transparent $1 -resize 72x72 -gravity center -extent 72x72 Images/xxhdpi/$2 # 2>/dev/null
-convert -density 300 -background transparent $1 -resize 96x96 -gravity center -extent 96x96 Images/xxxhdpi/$2 # 2>/dev/null
+convert -density 300 -background transparent $1 -resize "${w}"x"${h}" -gravity center -extent "${w}"x"${h}" Images/mdpi/$2 # 2>/dev/null
+w=$[ow + (ow / 2)]
+h=$[oh + (ow / 2)]
+convert -density 300 -background transparent $1 -resize "${w}"x"${h}" -gravity center -extent "${w}"x"${h}" Images/hdpi/$2 # 2>/dev/null
+w=$[ow * 2]
+h=$[oh * 2]
+convert -density 300 -background transparent $1 -resize "${w}"x"${h}" -gravity center -extent "${w}"x"${h}" Images/xhdpi/$2 # 2>/dev/null
+w=$[ow * 3]
+h=$[oh * 3]
+convert -density 300 -background transparent $1 -resize "${w}"x"${h}" -gravity center -extent "${w}"x"${h}" Images/xxhdpi/$2 # 2>/dev/null
+w=$[ow * 4]
+h=$[oh * 4]
+convert -density 300 -background transparent $1 -resize "${w}"x"${h}" -gravity center -extent "${w}"x"${h}" Images/xxxhdpi/$2 # 2>/dev/null
 echo 'Done'
+
+
+
+
+
+
+
+
